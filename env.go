0096c426924fea7656b0f722b316a5a46d773da8
Dana Woodman <dana@danawoodman.com>
2024-01-10 14:29:34 -0800

    Add docs to Getenv

--- env.go
+++ env.go
@@ -2,6 +2,10 @@ package gocrazy
 
 import "os"
 
+/*
+Getenv returns the value of the environment variable named by the key or the
+fallback value if the variable is not set.
+*/
 func Getenv(key, fallback string) string {
 	value, exists := os.LookupEnv(key)
 	if !exists {
