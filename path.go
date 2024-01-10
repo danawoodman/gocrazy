2db4426bd5f222a656d7f3865b195f2b7797c035
Dana Woodman <dana@danawoodman.com>
2024-01-10 14:29:20 -0800

    Fix ExpandHome for $HOME dirs

--- path.go
+++ path.go
@@ -8,16 +8,16 @@ import (
 
 // ExpandHome expands a path that starts with `~` or `$HOME` to the user's home directory.
 func ExpandHome(path string) (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
 	if strings.HasPrefix(path, "~/") {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			return "", err
-		}
 		return filepath.Join(home, path[2:]), nil
 	} else if strings.HasPrefix(path, "$HOME") {
-		home := os.Getenv("HOME")
 		if home != "" {
-			return filepath.Join(home, path[2:]), nil
+			return filepath.Join(home, path[5:]), nil
 		}
 
 	}
