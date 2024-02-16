0d0ab5b13d52cbee26e989000f6f24a47d370b06
Dana Woodman <dana@danawoodman.com>
2024-02-16 14:08:22 -0800

    Allow passing 0 value to ExecuteAtInterval to allow pausing

--- interval.go
+++ interval.go
@@ -8,6 +8,8 @@ import (
 ExecuteAtInterval runs a function at a specified interval, which can be dynamically
 updated at runtime by passing a new interval to the intervalChan channel.
 
+Passing an interval of 0 to the intervalChan channel will pause the ticker.
+
 Usage:
 
 	interval := 1 * time.Second
@@ -20,16 +22,42 @@ Usage:
 	intervalChan <- 2 * time.Second
 */
 func ExecuteAtInterval(fn func(), interval time.Duration, intervalChan <-chan time.Duration) {
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
+	var ticker *time.Ticker
+	if interval > 0 {
+		ticker = time.NewTicker(interval)
+		defer ticker.Stop()
+	}
 
 	for {
-		select {
-		case <-ticker.C:
-			fn()
-		case newInterval := <-intervalChan:
-			ticker.Stop()
-			ticker = time.NewTicker(newInterval)
+		if ticker != nil {
+			select {
+			case <-ticker.C:
+				fn()
+			case newInterval, ok := <-intervalChan:
+				if !ok {
+					return // channel closed, exit goroutine
+				}
+				if newInterval > 0 {
+					if ticker != nil {
+						ticker.Stop()
+					}
+					ticker = time.NewTicker(newInterval)
+				} else if newInterval == 0 {
+					if ticker != nil {
+						ticker.Stop()
+						ticker = nil // Pause ticker
+					}
+				}
+			}
+		} else {
+			// Ticker is paused, wait for new interval
+			newInterval, ok := <-intervalChan
+			if !ok {
+				return // channel closed, exit goroutine
+			}
+			if newInterval > 0 {
+				ticker = time.NewTicker(newInterval)
+			}
 		}
 	}
 }
